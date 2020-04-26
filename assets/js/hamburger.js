93fc4ec1ef7674ffac532915d02815c1bbb578c5
Administrator <jonrsample@gmail.com>
2020-04-26 17:13:40 +0200

    modified:   assets/css/hamburgers.css 	modified:   assets/css/main.css 	modified:   assets/js/hamburger.js 	modified:   index.html

--- assets/js/hamburger.js
+++ assets/js/hamburger.js
@@ -1,7 +1,18 @@
 var hamburger = document.querySelector(".hamburger");
+var menuMobile = document.querySelector("#allProjectsContent")
+
 // On click
 hamburger.addEventListener("click", function() {
   // Toggle class "is-active"
   hamburger.classList.toggle("is-active");
   // Do something else, like open/close menu
+  if(hamburger.classList.contains("is-active"))
+  {
+    menuMobile.style.display="block";
+  }
+  else
+  {
+    menuMobile.style.display="none";
+  }
+  
 });
